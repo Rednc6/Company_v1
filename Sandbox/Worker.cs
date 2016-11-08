dbfdaa2b5ced642b7daa901633fa0b3877de2ecc
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-11-08 12:51:18 +0100

    Opgave 2 solved.

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+        private string _skill;
+
+        public Worker(string skill, string name, int salaryPerMonth) : base(name, salaryPerMonth)
+        {
+            this._skill = skill;
+
+        } 
+        public string getSkill()
+        {
+            return _skill;
+        }
+
+    }
+}
